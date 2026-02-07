bce65c19c046534265b0021df69842d76efacdfb
ShbawgWater <bho778@mtroyal.ca>
2026-02-06 22:42:13 -0700

    first commit

---
+++ art-process.php
@@ -0,0 +1,79 @@
+<?php 
+
+include 'data.inc.php';
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo "<p>Error: This page must be accessed by submitting the form.</p>";
+    echo "<a href='ch12-proj21.php'>Return to form</a>";
+    exit;
+}
+$title = trim($_POST['title']);
+
+if ($title === '') {
+    echo "<p>Error: Title is required.</p>";
+    echo "<a href='ch12-proj21.php'>Return to form</a>";
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Chapter 12</title>
+    
+    <link rel="stylesheet" href="css/reset.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+</head>
+<body>
+  <?php include 'header.inc.php'; ?>
+    
+<main>
+    <section class="results">
+
+    
+    <table>
+  <caption class="results__caption">Art Work Saved</caption>
+
+  <tr>
+    <td class="results__label">Title</td>
+    <td class="results__value"><?= $_POST['title'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Description</td>
+    <td class="results__value"><?= $_POST['description'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Genre</td>
+    <td class="results__value"><?= $_POST['genre'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Subject</td>
+    <td class="results__value"><?= $_POST['subject'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Medium</td>
+    <td class="results__value"><?= $_POST['medium'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Year</td>
+    <td class="results__value"><?= $_POST['year'] ?></td>
+  </tr>
+
+  <tr>
+    <td class="results__label">Museum</td>
+    <td class="results__value"><?= $_POST['museum'] ?></td>
+  </tr>
+</table>
+
+    
+  
+    </section>
+</main>       
+</body>
+</html>
---
+++ data.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+$links = [
+   [ "url"=>"index.php", "label"=>"Home"],
+   [ "url"=>"about.php", "label"=>"About"],
+   [ "url"=>"works.php", "label"=>"Art Works"],
+   [ "url"=>"artist.php", "label"=>"Artists"]
+
+];
+
+$genres = [
+   'Abstract',
+   'Baroque',
+   'Gothic',
+   'Renaissance'
+];
+
+$subjects = [
+   'Animals',
+   'Landscape',
+   'People'
+];  
+
+/**
+ * Convert a string array to HTML <option> elements.
+ *
+ * @param array $items Array of strings to convert
+ * @return string HTML markup with <option> tags
+ */
+function array_to_options(array $items): string {
+   $html = "";
+   foreach ($items as $item) {
+      $html .= "<option value=\"{$item}\">{$item}</option>\n";
+   }
+   return $html;
+}
+ 
+?>
